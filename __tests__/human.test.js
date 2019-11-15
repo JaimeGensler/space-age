41c95ad48c5cdeb6ed00b336fbe8a635c553f5db
Jaime Gensler <jaimegensler0@gmail.com>
2019-11-15 15:37:34 -0800

    change method to getPlanetTime, add earth as acceptable input, add parameter for support for yearsLeft method

--- __tests__/human.test.js
+++ __tests__/human.test.js
@@ -19,26 +19,26 @@ describe('Human', () => {
   });
 
   //all remaining tests simply use (22 years) and should always pass
-  test('.getPlanetAge("Mercury") should return age in Mercury years', () => {
-    expect(reusableHuman.getPlanetAge('Mercury')).toEqual('91.31');
+  test('.getPlanetTime("Mercury") should return age in Mercury years', () => {
+    expect(reusableHuman.getPlanetTime('Mercury')).toEqual(91.31);
   });
-  test('.getPlanetAge("Venus") should return age in Venus years', () => {
-    expect(reusableHuman.getPlanetAge('Venus')).toEqual('35.71');
+  test('.getPlanetTime("Venus") should return age in Venus years', () => {
+    expect(reusableHuman.getPlanetTime('Venus')).toEqual(35.71);
   });
-  test('.getPlanetAge("Mars") should return age in Mars years', () => {
-    expect(reusableHuman.getPlanetAge('Mars')).toEqual('11.70');
+  test('.getPlanetTime("Mars") should return age in Mars years', () => {
+    expect(reusableHuman.getPlanetTime('Mars')).toEqual(11.70);
   });
-  test('.getPlanetAge("Jupiter") should return age in Jupiter years', () => {
-    expect(reusableHuman.getPlanetAge('Jupiter')).toEqual('1.85');
+  test('.getPlanetTime("Jupiter") should return age in Jupiter years', () => {
+    expect(reusableHuman.getPlanetTime('Jupiter')).toEqual(1.85);
   });
-  test('.getPlanetAge("Saturn") should return age in Saturn years', () => {
-    expect(reusableHuman.getPlanetAge('Saturn')).toEqual('0.75');
+  test('.getPlanetTime("Saturn") should return age in Saturn years', () => {
+    expect(reusableHuman.getPlanetTime('Saturn')).toEqual(0.75);
   });
-  test('.getPlanetAge("Uranus") should return age in Uranus years', () => {
-    expect(reusableHuman.getPlanetAge('Uranus')).toEqual('0.26');
+  test('.getPlanetTime("Uranus") should return age in Uranus years', () => {
+    expect(reusableHuman.getPlanetTime('Uranus')).toEqual(0.26);
   });
-  test('.getPlanetAge("Neptune") should return age in Neptune years', () => {
-    expect(reusableHuman.getPlanetAge('Neptune')).toEqual('0.13');
+  test('.getPlanetTime("Neptune") should return age in Neptune years', () => {
+    expect(reusableHuman.getPlanetTime('Neptune')).toEqual(0.13);
   });
 
   test('.yearsLeft(planet) should return either number of years left to live on a planet or number of years exceeding predictions', () => {
--- src/human.js
+++ src/human.js
@@ -14,17 +14,20 @@ export class Human {
     return this.ageInDays;
   }
 
-  getPlanetAge(planet) {
+  getPlanetTime(planet, days) {
+    if (days === undefined) days = this.ageInDays;
+
     const planets = {
       Mercury: 88,
       Venus: 225,
+      Earth: 365,
       Mars: 687,
       Jupiter: 4332,
       Saturn: 10759,
       Uranus: 30688,
       Neptune: 60200
     }
-    return (this.ageInDays/planets[planet]).toFixed(2);
+    return Math.round( (days/planets[planet])*100 )/100;
   }
 
   yearsLeft(planet) {
@@ -33,7 +36,7 @@ export class Human {
     if (this.ses = 'Upper') expectedAge += 5;
     if (this.ses = 'Lower') expectedAge -= 5;
 
-    const yearsLeft = (expectedAge - this.age).toFixed(2);
+    const yearsLeft = (expectedAge - this.age).toFixed(2); //can't manipulate a string (below)
     if (yearsLeft < 0) return (`surpassed life expectancy by ${-1 * yearsLeft} years'`);
     return (`${yearsLeft} Earth years left`);
   }
