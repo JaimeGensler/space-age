2d6eb4e4bc8642aa00ab1fb31ce380d737b4a9b7
Jaime Gensler <jaimegensler0@gmail.com>
2019-11-15 09:08:18 -0800

    add test for age in days

--- __tests__/human.test.js
+++ __tests__/human.test.js
@@ -1,13 +1,14 @@
-import { Human } from './../src/Human.js';
+import { Human } from './../src/human.js';
 
 describe('Human', () => {
-  let reusableHuman;
 
+  let reusableHuman;
   beforeEach(() => {
     reusableHuman = new Human(22);
   });
 
-  test('should correctly create a Human object that stores user age', () => {
+  test('should correctly create a Human object that stores age in years and days', () => {
     expect(reusableHuman.age).toEqual(22);
+    expect(reusableHuman.daysAge).toEqual(8030);
   });
 });
