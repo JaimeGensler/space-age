c33f3cd676d836cbac973f9ff504bdb9381f2a55
Jaime Gensler <jaimegensler0@gmail.com>
2019-11-15 11:32:44 -0800

    add logic for Mars year

--- src/human.js
+++ src/human.js
@@ -18,6 +18,8 @@ export class Human {
         return (this.ageInDays/88).toFixed(2);
       case 'Venus':
         return (this.ageInDays/225).toFixed(2);
+      case 'Mars':
+        return (this.ageInDays/687).toFixed(2);
     }
   }
 }
