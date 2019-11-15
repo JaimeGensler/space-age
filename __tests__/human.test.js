2af703c18ea0da4c222b2df27345f4233b623a47
Jaime Gensler <jaimegensler0@gmail.com>
2019-11-15 10:28:31 -0800

    create test for age in Mercury Years

--- __tests__/human.test.js
+++ __tests__/human.test.js
@@ -9,10 +9,15 @@ describe('Human', () => {
 
   test('should correctly create a Human object that stores age in years and days', () => {
     expect(reusableHuman.age).toEqual(22);
-    expect(reusableHuman.ageInDays).toEqual(8035.5);
+    expect(reusableHuman.ageInDays).toEqual(8035);
   });
 
   test('.setPreciseAge() should precisely re-set and return age in days based on birthday and current date', () => {
-    expect(reusableHuman.setPreciseAge(7, 25, 1997)).toEqual(8148); //this test will fail when tried from future dates (Nov. 15, 2019)
+    expect(reusableHuman.setPreciseAge(7, 25, 1997)).toEqual(8148); //this test will fail when tried from future dates (passed Nov. 15, 2019)
+  });
+
+  //all remaining tests simply use (22 years) and should always pass
+  test('.getPlanetAge("Mercury") should return age in Mercury years', () => {
+    expect(reusableHuman.getPlanetAge('Mercury')).toEqual('91.30');
   });
 });
